ddee03fc828e8e992a09e9fbf5fd7c4a575b53cf
Hyunhum <hyeonheum@naver.com>
2022-12-14 15:36:08 +0900

    feat: add test abi and address for example

---
+++ scripts/constants.js
@@ -0,0 +1,106 @@
+/* 
+    This is example of CONTRACT params.
+    Use your own CONTRACT address and abi!
+*/
+const CONTRACT_ADDRESS = "0x9e22f418fb8cd4f18ad56c29a6a25c29e5d21e48";
+const CONTRACT_ABI = [
+	{
+		"inputs": [
+			{
+				"internalType": "address[]",
+				"name": "_tokenHolders",
+				"type": "address[]"
+			},
+			{
+				"internalType": "uint256[]",
+				"name": "_balances",
+				"type": "uint256[]"
+			}
+		],
+		"name": "batchTransfer",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_tokenAddress",
+				"type": "address"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "token",
+		"outputs": [
+			{
+				"internalType": "contract IERC20",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+module.exports = {
+    CONTRACT_ADDRESS: CONTRACT_ADDRESS,
+	CONTRACT_ABI: CONTRACT_ABI
+};
